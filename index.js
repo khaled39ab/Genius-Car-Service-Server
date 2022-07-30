200d4b14fea4911358cbeda3c53d8247a8bc06fa
khaled39ab <khaled39ab@gmail.com>
2022-07-31 00:20:36 +0600

    send data from server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectID } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -13,18 +13,32 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.exvps.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('Db connected');
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+    try {
+        const serviceCollection = client.db('geniusCarService').collection('service');
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        });
+
+        
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
 
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('Server is running')
 });
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log('Port is listening');
 })
