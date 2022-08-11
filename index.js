2f6c82ed6598bd3639495feb85401fb10913b992
khaled39ab <khaled39ab@gmail.com>
2022-08-11 23:48:44 +0600

    create jwt token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -19,6 +20,16 @@ async function run() {
         const serviceCollection = client.db('geniusCarService').collection('service');
         const orderCollection = client.db('geniusCarService').collection('order')
 
+        //auth 
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken })
+        })
+
+        // service api
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
@@ -33,14 +44,14 @@ async function run() {
             res.send(service);
         });
 
-        // post
+        // post service
         app.post('/service', async (req, res) => {
             const newService = req.body;
             const result = await serviceCollection.insertOne(newService);
             res.send(result);
         });
 
-        // delete
+        // delete a service
         app.delete('/service/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
