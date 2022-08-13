549c6de363dd313b737f02f97b1803cadb255d24
khaled39ab <khaled39ab@gmail.com>
2022-08-14 01:48:49 +0600

    Heroku setup and deploy steps

---
+++ __server-deploy.js
@@ -0,0 +1,18 @@
+/* 
+node heroku deploy steps..
+
+---------------------------------
+One time for computer
+---------------------------------
+1- Create an account in heroku
+2- install heroku cli
+3- cmd line-> heroku login
+
+----------------------------------
+One time for each project
+----------------------------------
+1- cmd line-> heroku create
+2- make sure git cmd update(git add. -> git commit -m "" -> git push)
+3- cmd line-> git push heroku main
+
+*/
